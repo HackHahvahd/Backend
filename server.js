32cda1f128949984e5640fba07ddbe4a49fa450a
Chris <czw@mit.edu>
2015-11-14 11:29:12 -0500

    Working stuff

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var app = express();
 
-var io = require('socket.io').listen(app);
-
 app.get('/main',function(req,res){
 res.sendFile(__dirname + '/main.html');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
 var host = server.address().address;
 var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});
-
-io.sockets.on('connection', function(socket){
-  
 });
