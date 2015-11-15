1121ffc6c6fc041baa470a20112f25d41b6a304a
Brandon Wang <bdnwang@mit.edu>
2015-11-15 07:22:22 -0500

    working

--- NLP.py
+++ NLP.py
@@ -3,11 +3,22 @@ from nltk import word_tokenize
 from nltk.corpus import wordnet as wn
 import nltk, pprint, re
 import numpy
+import json, webbrowser
 
-keywords = ["computer", "science", "programming", "python", "java", "engineering"]
-keywords2 = ["cheese", "thighs", "vault", "fallout", "four", "smash"]
+#Read nonprofits json
+jList = []
+with open("nonprofits.json") as json_file:
+    json_data = json.load(json_file)
+
+for i in range(len(json_data)):
+    jdata = json_data[i]["keywords"]
+    jdataClrd = re.sub('[^a-zA-Z0-9-_*.\n?!]', ' ', jdata)
+    jtokens = word_tokenize(jdataClrd)
+    jtext = nltk.Text(jtokens)
+    jList.append(jtext)
+
 #Read text file outputted from OCR (java)
-with open ("C:\\Users\\Alan\\Documents\\Python\\HackHarvard\\SampleResumes\\ResumeText.txt", "r") as txtfile:
+with open ("test.txt", "r") as txtfile:
     data=txtfile.read()
 
 dataClrd = re.sub('[^a-zA-Z0-9-_*.\n?!]', ' ', data)
@@ -20,28 +31,36 @@ ttext = nltk.pos_tag(text)
 
 #Find Proper Nouns
 NNList = []
-VBList = []
 numSkills = 0
 for i in range(len(ttext)):
-	if "NNP" == ttext[i][1]:
+	if "NNP" == ttext[i][1] or "VB" in ttext[i][1]:
 		NNList.append(ttext[i][0])
 
-"""print "Nouns"
-for i in range(len(NNList)):
-	print NNList[i]"""
-
 #compare similarity
-sim = 0
-for i in range(len(NNList)):
-	try:
-		temp = wn.synset(NNList[i]+".n.01")
-		for j in range(len(keywords)):
-			keyword = wn.synset(keywords[j]+".n.01")
-			wordSim = temp.path_similarity(keyword)
-			if(wordSim >= 0.80):
-				sim = sim + wordSim*1000
-			else:
-				sim = sim + wordSim
+
+sim = []
+for k in range(len(json_data)):
+    sim.append(0)
+    for i in range(len(NNList)):
+        try:
+            temp = wn.synset(NNList[i]+".n.01")
+            for j in range(len(jList[k])):
+                keyword = wn.synset(jList[k][j]+".n.01")
+                wordSim = temp.path_similarity(keyword)
+                if(wordSim >= 0.80):
+                    sim[k] = sim[k] + wordSim*1000
+                else:
+                    sim[k] = sim[k] + wordSim
 	except:
 		pass
-print "similarity: ", sim
+#print "similarity: ",sim[0],sim[1]
+
+json_out = [x for (y,x) in sorted(zip(sim,json_data),reverse = True)]
+
+f = open('list.html','w')
+message = """<html><head></head><body><ul>"""
+for x in range(len(json_out)):
+    message = message + "<li>" + str(json_out[x]["id"]) + " " + str(json_out[x]["keywords"]) + " " + str(json_out[x]["blurb"]) + " " + str(json_out[x]["thumbnail_path"]) + "</li>"
+message = message + "</ul>"
+f.write(message)
+f.close()
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
-
+var spawn = require('child_process').spawn;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var multer = require('multer');
 var done = false;
+//var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+var path = require('path');
+var config = require('./config');
+var https = require('https');
+// var io = require('socket.io').listen(app);
+// var socket = io.connect('http://localhost:3000');
 
 //configure express to use body-parser as middle-ware
-
+//the
 
 //var io = require('socket.io').listen(app);
 var fs = require('fs');
@@ -17,13 +25,16 @@ var util = require('util');
 var querystring = require('querystring');
 var azure = require('azure-storage');
 
+// app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/submit',function(req,res){
 res.sendFile(__dirname + '/submit.html');
 });
 
 app.get('/register',function(req,res){
-  res.sendFile(__dirname + '/interface/register.html');
+  res.sendFile(__dirname + '/register.html');
 });
 
 //multer stuff
@@ -36,23 +47,52 @@ onFileUploadStart: function (file) {
 },
 onFileUploadComplete: function (file) {
   console.log(file.fieldname + ' uploaded to  ' + file.path)
+
+//   java -cp "pdfbox-1.8.10.jar;commons-logging-1.2.jar;fontbox-1.8t
+// 10.jar;" ReadText pdf.pdf pdftext.txt
+
+  var cmd = "java -cp pdfbox-1.8.10.jar;commons-logging-1.2.jar;fontbox-1.8.10.jar; ReadText " + file.path + " test.txt"
+  child = exec(cmd, function (error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+
+
+  getNonprofits(function(responseString){
+    fs.writeFile("nonprofits.json",responseString, function(err){
+      if (err){
+        return console.log(err);
+      }
+      else{
+        console.log("Succesfully created nonprofits.json!");
+      }
+      var cmd2 = "python nlp.py";
+      child = exec(cmd2, function(error, stdout,stderr){
+        console.log("Finished NLP");
+        console.log(stdout);
+        fs.readFile('list.html','utf8',function(err,contents){
+        //  socket.emit("results",contents)
+        })
+        //socket.emit("results",stdout);
+      });
+    });
+  });
+  console.log("almost there");
   done=true;
 
-  io.emit('processing-pdf');
-  //run alans pdf PROGRAM
-
-  //run alans nlp PROGRAM
-  //get nonprofits from azure database
-
-  //return data to browser
-  socket.emit('results', data);
+});
 }
 }));
 
+
+
 app.post('/api/photo',function(req,res){
   if(done==true){
+    console.console.log("should be done");
     console.log(req.files);
-    res.end("File uploaded.");
+    res.sendFile(__dirname + '/list.html');
   }
 });
 
@@ -113,14 +153,13 @@ app.get('/submit/nonprofit', function(req, res){
   req.end();
 });
 
+// app.get('/*', function(req,res){
+//  res.sendFile(req.params[0]);
+// });
 
 var getNonprofits = function(callback){
 
-    var entry = {
-      id: nonprofitName,
-      keywords: keywords,
-      blurb: blurb,
-      thumbnail_path: null //TODO
+    var entry = { //TODO
 
     };
 
@@ -152,14 +191,19 @@ var getNonprofits = function(callback){
             responseString += data;
         });
 
-        res.on('end', callback(responseString));
+        res.on('end', function(){
+          console.log("Got response from Azure!");
+          callback(responseString)
+        }
+
+        );
     });
 
     req.on('error', function (e) {
         // TODO: handle error.
     });
 
-    req.write(userString);
+    req.write(entryString);
     req.end();
 
 }
@@ -204,12 +248,29 @@ io.on('connection',function(socket){
 
 });
 
-
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+  // getNonprofits(function(responseString){
+  //   fs.writeFile("nonprofits.json",responseString, function(err){
+  //     if (err){
+  //       return console.log(err);
+  //     }
+  //     else{
+  //       console.log("Succesfully created nonprofits.json!");
+  //     }
+  //     var cmd2 = "python nlp.py";
+  //     child = exec(cmd2, function(error, stdout,stderr){
+  //       console.log("Finished NLP");
+  //       console.log(stdout);
+  //       socket.emit("results",stdout);
+  //     });
+  //   });
+  // });
+
+      // var child = spawn('java', ['TestJ', '/upload/v11.pdf', 'test.txt']);
+  // executes `pwd`
 
 });
